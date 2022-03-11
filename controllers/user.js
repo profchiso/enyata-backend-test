7180158d353cb1a2e953a75a152ed0aa42e0ceb6
Okorie Chinedu <okoriechinedusunday@gmail.com>
2022-03-11 21:30:39 +0100

    fix some issues

--- controllers/user.js
+++ controllers/user.js
@@ -2,8 +2,33 @@ const { validationResult } = require('express-validator');
 const { hashUserPassword } = require("../utils/passwordHashing")
 const db = require("../models/index")
 
-exports.getAllUser = async(req, res) => {}
-exports.getAuser = async(req, res) => {}
+exports.getAllUser = async(req, res) => {
+    try {
+        const users = await db.User.findAll({ where: req.query, });
+        res.status(200).json({ message: "Users Fetched", statuscode: 200, data: { users } });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Something went wrong", statuscode: 500, errors: [{ message: error.message || "internal server error" }] })
+
+    }
+}
+exports.getAuser = async(req, res) => {
+    try {
+
+        const { id } = req.params;
+        const user = await db.User.findOne({ where: { id } });
+        if (!user) {
+            return res.status(404).json({ message: "User not found", statuscode: 404, });
+
+        }
+
+        res.status(200).json({ message: "User Fetched", statuscode: 200, data: { user } });
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({ message: "Something went wrong", statuscode: 500, errors: [{ message: error.message || "internal server error" }] })
+
+    }
+}
 exports.createUser = async(req, res) => {
     try {
         const errors = validationResult(req);
@@ -11,17 +36,17 @@ exports.createUser = async(req, res) => {
             return res.status(400).json({ errors: errors.array() });
         }
         const { name, email, password } = req.body
-            //HASH USER PASSWORD
+
         let hashedPassword = await hashUserPassword(password)
 
-        //CHECK IF USER ALREADY EXIST
+
         const existingUser = await db.User.findOne({ where: { email } });
 
         if (existingUser) {
             return res.status(400).json({ message: `Email already in use`, statuscode: 400, errors: [{ message: `Email already in use` }] });
 
         }
-        const createdUser = await db.User.create({ name, email, password });
+        const createdUser = await db.User.create({ name, email, password: hashedPassword });
         console.log(createdUser)
         res.status(201).send({ message: "User created", statuscode: 201, data: { user: createdUser } });
 
@@ -32,5 +57,20 @@ exports.createUser = async(req, res) => {
 
     }
 }
-exports.updateUser = async(req, res) => {}
+exports.updateUser = async(req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await db.User.findOne({ where: { id } });
+        if (!user) {
+            return res.status(404).json({ message: "User not found", statuscode: 404, errors: [{ message: "User not found" }] })
+
+        }
+        delete req.body.password
+        const updatedUser = await db.User.update(req.body, { where: { id }, returning: true, plain: true })
+        res.status(200).json({ message: `User info updated`, statuscode: 200, data: { user: updatedUser[1] } });
+
+    } catch (error) {
+
+    }
+}
 exports.deleteUser = async(req, res) => {}
